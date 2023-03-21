ad1b0d0bd24bba4a9d78de984990e2d0f50de4b9
Matthias Koch <ithrowexceptions@gmail.com>
2023-03-21 02:53:07 +0100

    asdf

--- build/Build.cs
+++ build/Build.cs
@@ -85,7 +85,7 @@ class Build : NukeBuild
         {
             var directory = NewTipDirectories.First();
             FileSystemTasks.MoveDirectoryToDirectory(directory, TipsDirectory);
-            NewTipDirectory = directory / directory.Name;
+            NewTipDirectory = TipsDirectory / directory.Name;
 
             var remote = $"https://{GitHubActions.Actor}:{GitHubActions.Token}@github.com/{GitHubActions.Repository}";
             Git($"remote set-url origin {remote.DoubleQuote()}");
